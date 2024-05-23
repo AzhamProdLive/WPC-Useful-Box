5057005d8da55ebb9bc5bbd4a46400804acb56d5
WindowsTools2077 <135960690+WindowsTools2077@users.noreply.github.com>
2024-05-23 04:12:34 -0700

    Add files via upload

---
+++ Scripts/Files/Other/Regedit backup.cmd
@@ -0,0 +1,20 @@
+@echo off
+color c
+cls
+cd desktop
+reg EXPORT "HKEY_CLASSES_ROOT" Root.reg
+color b
+reg EXPORT "HKEY_CURRENT_USER" User.reg
+color 8
+reg EXPORT "HKEY_LOCAL_MACHINE" Machine.reg
+color d
+reg EXPORT "HKEY_USERS" Users.reg
+color e
+reg EXPORT "HKEY_CURRENT_CONFIG" Config.reg 
+
+
+@echo                    Made By epicZone <3
+pause /t 3
+
+
+4995*0.6=2997DH +560=3557DH
